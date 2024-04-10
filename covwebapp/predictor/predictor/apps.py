f46e900972579018966440619756202714387899
TRISHA <trish2582@gmail.com>
2024-04-11 01:32:53 +0530

    add

--- covwebapp/predictor/predictor/apps.py
+++ covwebapp/predictor/predictor/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HomeConfig(AppConfig):
+class PredictorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'home'
+    name = 'predictor'
--- covwebapp/predictor/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    context={'a':1}
-    return render(request,'index.html',context)
